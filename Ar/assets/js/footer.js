e649153bd13e5930ddd9d2f5b3ccb3f3cbe60feb
Ammar Ahmed <ammarahmed132004@outlook.com>
2025-06-26 06:16:59 +0300

    en

---
+++ Ar/assets/js/footer.js
@@ -0,0 +1,97 @@
+let foot = `
+  <div id="preloader"></div>
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center">
+    <i class="bi bi-arrow-up-short"></i>
+  </a>
+  <div class="app_web">
+    <a href="https://www.facebook.com" target="_blank" class="social-btn facebook">
+      <i class="fab fa-facebook-f"></i>
+    </a>
+    <a href="https://www.instagram.com" target="_blank" class="social-btn instagram">
+      <i class="fab fa-instagram"></i>
+    </a>
+    <a href="https://www.tiktok.com" target="_blank" class="social-btn tiktok">
+      <i class="fab fa-tiktok"></i>
+    </a>
+    <a href="https://twitter.com" target="_blank" class="social-btn twitter">
+      <i class="fab fa-twitter"></i>
+    </a>
+    <a href="https://wa.me/+201010408447" target="_blank" class="social-btn whatsapp">
+      <i class="fab fa-whatsapp"></i>
+    </a>
+    <a href="https://www.youtube.com" target="_blank" class="social-btn youtube">
+      <i class="fab fa-youtube"></i>
+    </a>
+  </div>
+
+  <footer id="footer" class="footer dark-background">
+    <div class="container footer-top">
+      <div class="row gy-4">
+        <div class="col-lg-4 col-md-6 footer-about">
+          <a href="index.html" class="logo d-flex align-items-center">
+            <span class="sitename">الوراق</span>
+          </a>
+          <div class="footer-contact pt-3">
+            <p>نحن نؤمن بأن الجودة والابتكار هما الأساس لتقديم منتجات ترضي جميع الأذواق. نحرص دائمًا على أن نكون في المقدمة.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>روابط سريعة</h4>
+          <ul>
+            <li><a href="#">الرئيسية</a></li>
+            <li><a href="#">من نحن</a></li>
+            <li><a href="#">المنتجات</a></li>
+            <li><a href="#">تواصل معنا</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>روابط تهمك</h4>
+          <ul>
+            <li><a href="#">الإعلانات</a></li>
+            <li><a href="#">الفعاليات</a></li>
+            <li><a href="#">المدونة</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-4 col-md-6 footer-links">
+          <h4>تواصل معنا</h4>
+          <div class="footer-contact pt-3">
+            <p>شارع آدم 108</p>
+            <p>نيويورك، NY 535022</p>
+            <p class="mt-3"><strong>الهاتف:</strong> <span>+1 5589 55488 55</span></p>
+            <p><strong>البريد الإلكتروني:</strong> <span>info@example.com</span></p>
+          </div>
+          <div class="social-links d-flex mt-4">
+            <a href="https://youtube.com/@alwarraq-egypt?feature=shared" target="_blank">
+              <i class="bi bi-youtube"></i>
+            </a>
+            <a href="https://www.facebook.com/waraqcheese?mibextid=ZbWKwL" target="_blank">
+              <i class="bi bi-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/waraqcheese?igsh=c3UydnJ1MGJodHlz" target="_blank">
+              <i class="bi bi-instagram"></i>
+            </a>
+            <a href="https://www.tiktok.com/@waraqcheese?_t=ZS-8xKf9GyFJa9&_r=1" target="_blank">
+              <i class="bi bi-tiktok"></i>
+            </a>
+            <a href="https://twitter.com/waraqcheese" target="_blank">
+              <i class="bi bi-twitter"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container copyright text-center mt-4">
+      <p>© 2025 <span>جميع الحقوق محفوظة</span> <strong class="px-1 sitename">الوراق</strong></p>
+      <div class="credits">
+        تصميم وبرمجة <a href="https://queenarnor.com/en" target="_blank">Queenar</a>
+      </div>
+    </div>
+  </footer>
+`;
+
+let footer_ = document.getElementById("footer_");
+footer_.innerHTML = foot;
--- Ar/assets/js/header.js
+++ Ar/assets/js/header.js
@@ -1,37 +1,31 @@
-let head = `  <header id="header" class="header d-flex align-items-center fixed-top">
+let head = `  
+  <header id="header" class="header d-flex align-items-center fixed-top">
     <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-center">
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li class="bg ptn"><a href="index.html" class="active">Home</a></li>
-          <li class="bg ptn"><a href="about.html">About</a></li>
-          <li class="bg ptn"><a href="projects.html">Products</a></li>
-          <li class="bg ptn"><a href="advertise.html">Advertisements</a></li>
+          <li class="bg ptn"><a href="index.html" class="active">الرئيسية</a></li>
+          <li class="bg ptn"><a href="about.html">من نحن</a></li>
+          <li class="bg ptn"><a href="projects.html">المنتجات</a></li>
+          <li class="bg ptn"><a href="advertise.html">الإعلانات</a></li>
           <a href="/" class="logo d-flex align-items-center">
             <img src="../img/logo.png" class="main_logo">
           </a>
-          <li class="bg ptn"><a href="event.html">Events</a></li>
-          <li class="bg ptn"><a href="blog.html">Blog</a></li>
-          <li class="bg ptn"><a href="contact.html">Contact</a></li>
-
-
+          <li class="bg ptn"><a href="event.html">الفعاليات</a></li>
+          <li class="bg ptn"><a href="blog.html">المدونة</a></li>
+          <li class="bg ptn"><a href="contact.html">تواصل معنا</a></li>
           <li class="bg ptn">
             <a href="contact.html">
-              <i class="bi bi-globe"></i> Ar
+              <i class="bi bi-globe"></i> En
             </a>
           </li>
-
-
         </ul>
-
 
         <div class="d-flex">
           <a href="/" class="logo d-flex align-items-center">
             <img src="../img/logo.png" class="main_logo logo_phone">
           </a>
           <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-
         </div>
-
       </nav>
     </div>
   </header>
